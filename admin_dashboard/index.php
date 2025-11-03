8ab382513e46c24d6392719d7b7f319506699c6d
angelica-ctrl <angelpitaramirez1@gmail.com>
2025-11-03 18:26:40 +0800

    Create index.php

---
+++ admin_dashboard/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>E-VoiceSpartans Tracker</title>
+  <?php include 'includes/header.php'; ?>
+</head>
+<body>
+  <?php include 'includes/dashboard.php'; ?>
+  <?php include 'includes/responses.php'; ?>
+  <?php include 'includes/profile.php'; ?>
+  <?php include 'includes/settings.php'; ?>
+  <?php include 'includes/scripts.php'; ?>
+</body>
+</html>
