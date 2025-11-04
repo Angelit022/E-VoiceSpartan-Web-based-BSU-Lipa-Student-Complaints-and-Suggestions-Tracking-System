28dda1ea50f55f2fcdcdc5981a4889880df4d43a
angelica-ctrl <angelpitaramirez1@gmail.com>
2025-11-04 20:54:22 +0800

    Create script.js

---
+++ login_signup/js/script.js
@@ -0,0 +1,22 @@
+const loginForm = document.getElementById('loginForm');
+const loginSection = document.getElementById('login-section');
+const signupSection = document.getElementById('signup-section');
+const showSignup = document.getElementById('showSignup');
+const backToLogin = document.getElementById('backToLogin');
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  window.location.href = "homepage.html";
+});
+
+showSignup.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginSection.style.display = 'none';
+  signupSection.style.display = 'flex';
+});
+
+backToLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  signupSection.style.display = 'none';
+  loginSection.style.display = 'flex';
+});
